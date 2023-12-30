da1de4b5f294abe49e7b5e062aae5c892f0d4df0
CarTmannn <muhammadraihanxz12@gmail.com>
2023-12-30 16:35:17 +0800

    dart_library

---
+++ example/hello.dart
@@ -0,0 +1,9 @@
+import 'package:dart_library/hello.dart';
+
+void main(){
+   print(sayHello("rehan"));
+
+   var customer = Customer();
+   var categoory = Category();
+   var product = Product();
+}
